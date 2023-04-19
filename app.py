cc1db5a4c43f377b59b0675118ea15eefacdcaf4
cyberassemble <124307071+cyberassemble@users.noreply.github.com>
2023-04-19 09:03:28 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from pypasser import reCaptchaV3
 
 app = Flask(__name__)
+reCaptcha_response = reCaptchaV3('https://www.google.com/recaptcha/api2/anchor?ar=1&k=6LeBf0gaAAAAAOR1eq0Ypw6VKwNL3DS9zxR3P8mD&co=aHR0cHM6Ly93d3cuZGVzdGlueXJlc2N1ZS5vcmcuYXU6NDQz&hl=en&v=vkGiR-M4noX1963Xi_DB0JeI&size=invisible&cb=s6qcpjws25j9')
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "reCaptcha_response"
